b31272452f169069bf1ca1f4a75dec62a4290150
williamhickok11 <williamhickok11@gmail.com>
2016-01-22 16:07:05 -0600

    some objects

---
+++ js.js
@@ -0,0 +1,18 @@
+var movie03 = {
+title: "<h1>Missing Michael</h1>",
+imageURL: "<img src='http://ecx.images-amazon.com/images/I/91GAgvnO3AL._UY200_RI_UY200_.jpg'>",
+bgSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+txtSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option>        <option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+description: "<p>A horrible movie with spectacular music. The plot is not cohesive the acting is atrocious, but man is the music out of this world.</p>",
+price: "<p>$.49</p>"
+};
+
+
+var movie04 = {
+title: "<h1>Babe</h1>",
+imageURL: "<img src='http://ecx.images-amazon.com/images/I/91GAgvnO3AL._UY200_RI_UY200_.jpg'>",
+bgSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+txtSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option>        <option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+description: "<p>This is about a pig. This is about a pig This is about a pigThis is about a pig This is about a pig This is about a pig This is about a pig</p>",
+price: "<p>$.49</p>"
+};
