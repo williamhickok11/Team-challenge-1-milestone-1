44c1922753aefd98a7593ee232ca7142e96e15d4
williamhickok11 <williamhickok11@gmail.com>
2016-01-25 11:52:03 -0600

    added some functions

--- js.js
+++ js.js
@@ -3,8 +3,10 @@ var movieArray = [];
 var movie0 = {
 	title: "<h1>A New Hope</h1>",
 	imageURL: "<img src='http://ecx.images-amazon.com/images/I/91GAgvnO3AL._UY200_RI_UY200_.jpg'>",
-	bgSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
-	txtSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+	bgSelector: '<select id="bgSelector0"
+   name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+	txtSelector: '<select id="txtSelector0"
+   name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
 	description: "<p>Young farm boy Luke Skywalker is thrust into a galaxy of adventure when he intercepts a distress call from the captive Princess Leia. The event launches him on a daring mission to rescue her from the clutches of Darth Vader and the Evil Empire.</p>",
 	price: "<p>$19.99</p>"
 };
@@ -12,8 +14,10 @@ var movie0 = {
 var movie1 = {
 	title: "<h1>The Empire Strikes Back</h1>",
 	imageURL: "<img src='http://ecx.images-amazon.com/images/I/91MYvGBxoCL._UY200_RI_UY200_.jpg'>",
-	bgSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Background Color</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
-	txtSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Text Color</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+	bgSelector: '<select id="bgSelector1"
+   name="type"><option selected="selected" disabled="disabled">Choose Background Color</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+	txtSelector: '<select id="txtSelector1"
+   name="type"><option selected="selected" disabled="disabled">Choose Text Color</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
 	description: "<p>YThe Rebels scatter after the Empire attacks their base on the ice planet Hoth. Han Solo and Princess Leia are pursued by Imperials, while Luke trains with Jedi Master Yoda. Luke must battle Darth Vader and learns the shocking truth of his past.</p>",
 	price: "<p>$29.99</p>"
 };
@@ -21,8 +25,10 @@ var movie1 = {
 var movie2 = {
 	title: "<h1>Missing Michael</h1>",
 	imageURL: "<img src='http://ecx.images-amazon.com/images/I/91GAgvnO3AL._UY200_RI_UY200_.jpg'>",
-	bgSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
-	txtSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option>        <option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+	bgSelector: '<select id="bgSelector2"
+   name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+	txtSelector: '<select id="txtSelector2"
+   name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option>        <option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
 	description: "<p>A horrible movie with spectacular music. The plot is not cohesive the acting is atrocious, but man is the music out of this world.</p>",
 	price: "<p>$.49</p>"
 };
@@ -30,8 +36,10 @@ var movie2 = {
 var movie3 = {
 	title: "<h1>Babe</h1>",
 	imageURL: "<img src='http://ecx.images-amazon.com/images/I/91GAgvnO3AL._UY200_RI_UY200_.jpg'>",
-	bgSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
-	txtSelector: '<select class="destination-type" name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option>        <option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+	bgSelector: '<select id="bgSelector3"
+   name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option><option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
+	txtSelector: '<select id="txtSelector3"
+   name="type"><option selected="selected" disabled="disabled">Choose Destination</option><option>Oranjestad, Aruba</option><option>Buenos Aires, Argentina</option><option>Marrakech, Morocco</option>        <option>Bangkok, Thailand </option><option>Singapore</option><option>Hanoi, Vietnam</option><option>Dublin, Ireland</option><option>Queenstown, New Zealand</option><option>Bora Bora</option>',
 	description: "<p>This is about a pig. This is about a pig This is about a pigThis is about a pig This is about a pig This is about a pig This is about a pig</p>",
 	price: "<p>$.49</p>"
 };
@@ -41,7 +49,21 @@ movieArray.push(movie1);
 movieArray.push(movie2);	
 movieArray.push(movie3);	
 
+
 var count = -1;
+
+function deleteButton(button) {
+  button.parentNode.parentNode.removeChild(button.parentNode);
+  return false;
+};
+
+//*** CHANGE THE BACKGROUND FUNCTION ***//
+function changeBG(this) {
+  var e = document.getElementById("ddlViewBy");
+  var strUser = e.options[e.selectedIndex].value;
+};
+
+
 
 //*** ADD CARDS TO THE DOM ***//
 function addProduct() {
@@ -64,10 +86,12 @@ function addProduct() {
   
   //*** ADD CONTENT TO THE CARDS ***//
   var currentArticle = document.getElementById("movie" + count);
+  currentArticle.innerHTML += '<button onclick="deleteButton(this)">Delete</button>'
 	currentArticle.innerHTML += movieArray[count].title;
 	currentArticle.innerHTML += movieArray[count].imageURL;
 	currentArticle.innerHTML += movieArray[count].bgSelector;
 	currentArticle.innerHTML += movieArray[count].txtSelector;
+  currentArticle.innerHTML += '<button onclick="changeBG(this)">Submit</button>';
 	currentArticle.innerHTML += movieArray[count].description;
 	currentArticle.innerHTML += movieArray[count].price;
 };
